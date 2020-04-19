c04d16bde1e7c81a760efadcde7ee63ce9e2ca53
Vlada <bogitronic@gmail.com>
2020-04-19 15:51:49 +0200

    tacka

---
+++ Bogi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bogi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Početak je težak!");
+
+            Console.ReadLine();
+        }
+    }
+}
+o
